ed20d23e0471e3ef92301cfb7aadbc8964fc9e06
Vonn <shevonnaboyd@gmail.com>
2019-08-23 19:04:40 -0700

    basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
-import Books from "./pages/Books";
-import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Books from "./pages/Info";
+import Main from "./pages/index";
+import Dash from "./pages/Dashboard";
+import QR from "./pages/QR";
+import Navbar from "./components/Nav";
+// import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Books />
-    </div>
+    <Router>
+      <div>
+        <Navbar/>
+        <Wrapper>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/info" component={Books} />
+          <Route exact path="/QR" component={QR} />
+          <Route exact path="/dashboard" component={Dash} />
+        </Wrapper>
+        {/* <Footer /> */}
+      </div>
+    </Router>
   );
 }
 
--- client/src/pages/Info.js
+++ client/src/pages/Info.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import Jumbotron from "../../components/Jumbotron";
-import DeleteBtn from "../../components/DeleteBtn";
-import API from "../../utils/API";
-import { Col, Row, Container } from "../../components/Grid";
-import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
+import Jumbotron from "../components/Jumbotron";
+import DeleteBtn from "../components/DeleteBtn";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
 
 class Books extends Component {
   // Setting our component's initial state
